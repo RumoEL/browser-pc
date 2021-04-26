c2bc0958f5a540772cb12af9f499d394370e4230
RumoEL <tbr4ddeqrrz25wwrqstr5b1s1rvq@gmail.com>
2021-04-26 12:03:07 +0000

    add main class and plugins

---
+++ client-gui/src/main/java/com/github/rumoel/rumoel/browser/pc/client/init/InitBrowserClient.java
@@ -0,0 +1,10 @@
+package com.github.rumoel.rumoel.browser.pc.client.init;
+
+public class InitBrowserClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
